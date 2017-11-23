28924529561893e59513fd92eb6188f13418498d
scoutchorton <scoutchorton@yahoo.com>
2017-11-23 16:05:13 -0500

    Added code from Trinket (and debugged)

--- err.py
+++ err.py
@@ -1,30 +1,31 @@
+# -*- coding: utf-8 -*-
 from random import randint
 """
 ERROR CODES
 00.0 - Test Code
-     * Whoops. You're lucky to see this. +1 to you!
-     * This is a test. This is a test. This is a test.
-     * Easter egg!
+			* Whoops. You're lucky to see this. +1 to you!
+			* This is a test. This is a test. This is a test.
+			* Easter egg!
 01.# - Syntax
-    .1 - Space in code
-       * I bet you don't like walking over voids in the ground. Me either. Could ya' fix that for me?
-       * We encountered a space here. Who do you think I am, NASA or something?
-       * Hello ~spaces~, my old friend!
+		.1 - Space in code
+			 * I bet you don't like walking over voids in the ground. Me either. Could ya' fix that for me?
+			 * We encountered a space here. Who do you think I am, NASA or something?
+			 * Hello ~spaces~, my old friend!
 """
 def randItem(dic):
-  return dic[randint(0, len(dic)-1)]
-  
+	return dic[randint(0, len(dic)-1)]
+
 
 def errRet(errCod):
-  errCods = {
-    00.0: ["Test Error", 
-    ["Whoops. You're lucky to see this. +1 to you!", "Testing, testing, 1, 2, 3, testing, testing.", "Easter egg!"]],
-    01.1: ["Syntax - Space In Code",
-    ["I bet you don't like walking over voids in the ground. Me either. Could ya' fix that for me?", "We encountered a space here. Who do you think I am, NASA or something?", "Hello darkness, err, spaces, my old friend!"]]
-  }
-  if not (errCod in errCods.keys()):
-    print "ERR 00.0: " + errCods[00.0][0],
-    print randItem(errCods[00.0][1])
-  else:
-    print "ERR " + str(errCod) + ": " + errCods[errCod][0] + ":",
-    print randItem(errCods[errCod][1])
+	errCods = {
+		00.0: ["Test Error",
+		["Whoops. You're lucky to see this. +1 to you!", "Testing, testing, 1, 2, 3, testing, testing.", "Easter egg!"]],
+		01.1: ["Syntax - Space In Code",
+		["I bet you don't like walking over voids in the ground. Me either. Could ya' fix that for me?", "We encountered a space here. Who do you think I am, NASA or something?", "Hello darkness, err, spaces, my old friend!"]]
+	}
+	if not (errCod in errCods.keys()):
+		print "ERR 00.0: " + errCods[00.0][0],
+		print randItem(errCods[00.0][1])
+	else:
+		print "ERR " + str(errCod) + ": " + errCods[errCod][0] + ":",
+		print randItem(errCods[errCod][1])
--- interpreter.py
+++ interpreter.py
@@ -1,132 +1,123 @@
 # -*- coding: utf-8 -*-
 import err
+from time import sleep
+from os import system
 class interpreter(object):
-  def __init__(self, codeSource):
-      #Variables
-    self.codeFile = open(codeSource, 'r')
-    self.tempCode = self.codeFile.read()
-    self.codeFile.close()
-    self.tempCode = self.tempCode.split('\n')
-    self.code = []
+	def __init__(self, codeSource):
+			#Variables
+		self.codeFile = open(codeSource, 'r')
+		self.tempCode = self.codeFile.read()
+		self.codeFile.close()
+		self.tempCode = self.tempCode.split('\n')
+		self.code = []
 
-  def mov(self, l, d):
-    print l
-    #Movement
-    #If going south
-    if d == 2:
-      l[1] += 1
-    #If going north
-    elif d == 0:
-      l[1] -= 1
-    #If going east
-    elif d == 1:
-      l[0] += 1
-    #If going west
-    elif d == 3:
-      l[0] -= 1
-    print l
-    return l
+	def mov(self, l, d):
+		print l
+		#Movement
+		#If going south
+		if d == 2:
+			l[1] += 1
+		#If going north
+		elif d == 0:
+			l[1] -= 1
+		#If going east
+		elif d == 1:
+			l[0] += 1
+		#If going west
+		elif d == 3:
+			l[0] -= 1
+		print l
+		return l
 
-  def run(self):
-      ##############
-      #CODE LOADING#
-      ##############
+	def run(self):
+			##############
+			#CODE LOADING#
+			##############
 
-    #Transforms into array of arrays from just an array
-    for i in range(0, len(self.tempCode)):
-      tempLine = []
-      for x in self.tempCode[i]:
-        tempLine.append(x)
-      self.code.append(tempLine)
+		#Transforms into array of arrays from just an array
+		for i in range(0, len(self.tempCode)):
+			tempLine = []
+			for x in self.tempCode[i]:
+				tempLine.append(x)
+			self.code.append(tempLine)
 
-    #Removes last line in array to prevent an empty line at the end
-    temp = []
-    for i in range(0, len(self.code)-2):
-      temp.append(self.code[i])
-    self.code = temp
+		#Finds the length of the longest list (width of code)
+		codeWidth = 1
+		for i in self.code:
+			if len(i) > codeWidth:
+				codeWidth = len(i)
 
-    #Finds the length of the longest list (width of code)
-    codeWidth = 1
-    for i in self.code:
-      if len(i) > codeWidth:
-        codeWidth = len(i)
+		#Fills in with spaces so everything is the same width
+		for i in range(0, len(self.code)):
+			if len(self.code[i]) < codeWidth:
+				diff = codeWidth - len(self.code[i])
+			for s in range(0, diff):
+				self.code[i].extend(" ")
 
-    #Fills in with spaces so everything is the same width
-    for i in range(0, len(self.code)):
-      if len(self.code[i]) < codeWidth:
-        diff = codeWidth - len(self.code[i])
-        for s in range(0, diff):
-          self.code[i].extend(" ")
+			#############
+			#INTERPRETER#
+			#############
+		#Variables
+			#Direction
+			#0 north
+			#1 east
+			#2 south
+			#3 west
+			#			N/0
+			#			 |
+			#	W/3 	 	E/1
+			#		 	 |
+			#			S/2
+		dir = 2
+			#Location
+		loc = [0, 0]
+			#Buffer
+		buf = {"print":[], "if":[], "loop":[]}
+			#Goto
+		goto = [0, 0]
+			#Font
+		regu = '\033[0m'
+		bold = '\033[0;37;42m'
 
-    """
-    for i in self.code:
-      print str(i)[1:-1]
-    """
+		print """###########\n#EXECUTING#\n###########"""
+		while True:
+				#Printing code
+			system("clear")
+			for i in buf:
+				if buf[i] != []:
+					print i + ": " + str(buf[i])
+			for y in range(0, len(self.code)):
+				for x in range(0, len(self.code[y])):
+					if [x,y] == loc:
+						print bold + self.code[y][x] + regu,
+					else:
+						print self.code[y][x],
+				print ""
+			print "\n",
+			print "─"*(codeWidth*2),
+			print	"\n"
 
-      #############
-      #INTERPRETER#
-      #############
-    #Variables
-      #Direction
-        #0 north
-        #1 east
-        #2 south
-        #3 west
-        #      N/0
-        #       |
-        #  W/3--*--E/1
-        #       |
-        #      S/2
-    dir = 2
-      #Location
-    loc = [0, 0]
-      #Buffer
-    buf = {"print":[], "if":[], "loop":[]}
-      #Goto
-    goto = [0, 0]
-      #Font
-    regu = '\033[0m'
-    bold = '\033[0;37;42m'
+			loc = self.mov(loc, dir)
+			print loc
 
-    print """###########\n#EXECUTING#\n###########"""
-    while True:
-      #Printing code
-      print "\n"*25
-      for i in buf:
-        if buf[i] != []:
-          print i + ": " + str(buf[i])
-      for y in range(0, len(self.code)):
-        for x in range(0, len(self.code[y])):
-          if [x,y] == loc:
-            print bold + self.code[y][x] + regu,
-          else:
-            print self.code[y][x],
-        print ""
-      print "\n",
-      print "─"*(codeWidth*2),
-      print  "\n"
+			block = self.code[loc[1]][loc[0]]
+				#Interpretation
+			if (dir == 1) and (block == "("):
+				goto = loc
+				while block != ")":
+					self.mov()
+					buf["loop"][0] += block
+			elif block == "^":
+				dir = 0
+			elif block == ">":
+				dir = 1
+			elif block == "v":
+				dir = 2
+			elif block == "<":
+				dir = 3
+			elif block == " ":
+				err.errRet(01.0)
+				break
+			sleep(0.1)
 
-      loc = self.mov(loc, dir)
-      print type(loc)
-
-      block = self.code[loc[1]][loc[0]]
-    #Interpretation
-      if (dir == 1) and (block == "("):
-        goto = loc
-        while block != ")":
-          self.mov()
-          buf["loop"][0] += block
-
-      elif block == "^":
-        dir = 0
-      elif block == ">":
-        dir = 1
-      elif block == "v":
-        dir = 2
-      elif block == "<":
-        dir = 3
-      elif block == " ":
-        err.errRet(01.0)
-        break
-
-    print """##########\n#FINISHED#\n##########"""
+		print """##########\n#FINISHED#\n##########"""
